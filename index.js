bee68d2576576148438f509ea7cdbe1754785178
przempaczkowski <25874300+przempaczkowski@users.noreply.github.com>
2023-10-18 00:28:32 +0100

    beautiful buttons

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const main = document.querySelector('#main');
 const toolBox = document.querySelector('#tool-box');
 const grid = document.querySelector('#grid');
+const btnLow = document.querySelector('#low');
+const btnFullHd = document.querySelector('#fullHd');
+const btnFourK = document.querySelector('#fourk');
+const btnTop = document.querySelector('#top');
+const btnOff = document.querySelector('#off')
+const btnReset = document.querySelector('#reset')
 
-let gridSize = prompt("Set grid size. Be nice to your browser, pick something between 2 and 96", " ");
 
-        
+let gridSize = 0;
 function createGrid() {
-    grid.innerHTML = ''; // Clear the existing grid
-    const boxSize = `${24 / gridSize}rem`; // Calculate box size based on grid size
+    grid.innerHTML = ''; // clear the existing grid
+    const boxSize = `${24 / gridSize}rem`; // box size based on grid size
 
     for (let i = 0; i < gridSize * gridSize; i++) {
         const box = document.createElement('div');
@@ -17,15 +22,40 @@ function createGrid() {
         box.style.border = '1px solid pink';
         grid.appendChild(box);
     }
+
+    const boxes = document.querySelectorAll('.box'); //  select all elements with the class 'box'
+    boxes.forEach((box) => {
+        box.addEventListener('mouseover', function () {
+            box.classList.add('change');
+        });
+    });
 }
 
 createGrid();
 
-
-const boxes = document.querySelectorAll('.box'); //  select all elements with the class 'box'
-
-boxes.forEach((box) => {
-    box.addEventListener('mouseover', function () {
-        box.classList.add('change');
-    });
+btnLow.addEventListener('click', () => {
+    gridSize = 8;
+    createGrid(); 
 });
+
+btnFullHd.addEventListener('click', () => {
+    gridSize = 16;
+    createGrid(); 
+});
+
+btnFourK.addEventListener('click', () => {
+    gridSize = 32;
+    createGrid(); 
+});
+
+btnTop.addEventListener('click', () => {
+    gridSize = 64;
+    createGrid(); 
+});
+
+btnOff.addEventListener('click', () => {
+    gridSize = 0;
+    createGrid(); 
+});
+
+
