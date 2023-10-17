30dc272f8d62868dc2640f68dc6eb4627f2324b2
przempaczkowski <25874300+przempaczkowski@users.noreply.github.com>
2023-10-17 23:13:56 +0100

    resizable box

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ const main = document.querySelector('#main');
 const toolBox = document.querySelector('#tool-box');
 const grid = document.querySelector('#grid');
 
-let gridSize = 24;
+let gridSize = prompt("Set grid size. Be nice to your browser, pick something between 2 and 96", " ");
 
         
 function createGrid() {
+    grid.innerHTML = ''; // Clear the existing grid
+    const boxSize = `${24 / gridSize}rem`; // Calculate box size based on grid size
+
     for (let i = 0; i < gridSize * gridSize; i++) {
         const box = document.createElement('div');
         box.classList.add('box');
-        box.style.width = `${gridSize / 96}rem`;  // You can change this value for different box sizes
-        box.style.height = `${gridSize / 96}rem`; // You can change this value for different box sizes
-        box.style.border = 'solid black 1px';
+        box.style.width = boxSize;
+        box.style.height = boxSize;
+        box.style.border = '1px solid pink';
         grid.appendChild(box);
     }
 }
+
 createGrid();
 
 
-const boxes = document.querySelectorAll('.box'); // Use querySelectorAll to select all elements with the class 'box'
+const boxes = document.querySelectorAll('.box'); //  select all elements with the class 'box'
 
 boxes.forEach((box) => {
     box.addEventListener('mouseover', function () {
