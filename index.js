255891b579d9391b215b965c989ffbb58f44b16c
przempaczkowski <25874300+przempaczkowski@users.noreply.github.com>
2023-10-17 22:26:26 +0100

    16x16 grid with hover

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const main = document.querySelector('#main');
 const toolBox = document.querySelector('#tool-box');
 const grid = document.querySelector('#grid');
 
-let gridSize = 16;
+let gridSize = 24;
 
         
 function createGrid() {
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
         const box = document.createElement('div');
         box.classList.add('box');
-        box.style.width = '1rem';  // You can change this value for different box sizes
-        box.style.height = '1rem'; // You can change this value for different box sizes
+        box.style.width = `${gridSize / 96}rem`;  // You can change this value for different box sizes
+        box.style.height = `${gridSize / 96}rem`; // You can change this value for different box sizes
         box.style.border = 'solid black 1px';
         grid.appendChild(box);
     }
